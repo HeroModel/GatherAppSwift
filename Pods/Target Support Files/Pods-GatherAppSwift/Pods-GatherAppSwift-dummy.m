961be405fc9a8e7c7e61bdf4c17d18df870563e5
apple <977720477@qq.com>
2018-04-11 18:02:41 +0800

    添加网络请求

---
+++ Pods/Target Support Files/Pods-GatherAppSwift/Pods-GatherAppSwift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GatherAppSwift : NSObject
+@end
+@implementation PodsDummy_Pods_GatherAppSwift
+@end
---
+++ Pods/Target Support Files/Pods-GatherAppSwiftUITests/Pods-GatherAppSwiftUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GatherAppSwiftUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GatherAppSwiftUITests
+@end
